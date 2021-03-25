b2552ecf2c20e16cb5e7c1ce11c61e53f1d2db79
Pranjal <shindepranjal818@gmail.com>
2021-03-25 14:14:02 +0530

    Added header and footer

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
     //owl-carousel for events
-    $('.owl-carousel').owlCarousel({
+    $('.owl-one').owlCarousel({
         loop:true,
         autoplay: true,
         autoplayTimeout: 3000,
         dots: false,
         nav: true,
-
         responsiveClass:true,
         responsive:{
         0:{
@@ -18,11 +17,95 @@ $(document).ready(function(){
     }
     });
 
+    $('.owl-two').owlCarousel({
+        loop:true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        responsiveClass:true,
+        responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        }
+    } });
+    $('.owl-three').owlCarousel({
+        loop:true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        responsiveClass:true,
+        responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        }
+    }
+   
+    });
+    
+    $('.owl-four').owlCarousel({
+        loop:true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        responsiveClass:true,
+        responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        }
+    }
+   
+    });
+    
+    
     $(".btn").click(function() {
         $('html, body').animate({
             scrollTop: $("#explore").offset().top
         }, 2000);
     });
+
+    
     
 });
+
+function navSlide() {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+ 
+    burger.addEventListener("click", () => {
+        //Toggle Nav
+        nav.classList.toggle("nav-active");
+ 
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle("toggle");
+    });
+ 
+    // $(document).click(function(event) { if(!$(event.target).closest('.burger').length) { $(".nav-links is").fadeOut(); } });
+ 
+ }
+ 
+ 
+ 
+ navSlide();
+ 
 
